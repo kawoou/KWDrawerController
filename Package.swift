a58ecc968bb1e9c2ea25d5622f9904024e14dd06
Lismond Bernard <lismond@acst.com>
2023-04-14 10:12:47 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "KWDrawerController",
+            dependencies: ["RxSwift"],
             path: "DrawerController"
         )
     ]
