a23edb65a0eb04c966680f083dd2aa98fa329423
Lismond Bernard <lismond@acst.com>
2023-04-14 10:07:41 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "KWDrawerController",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(name: "KWDrawerController", targets: ["KWDrawerController"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", exact: "5.0.0")
     ],
     targets: [
         .target(
@@ -16,4 +19,3 @@ let package = Package(
         )
     ]
 )
-
